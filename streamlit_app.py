42141a428871d1a2b89cace799addc8d17a901b6
twisted-realms-jess <166146094+twisted-realms-jess@users.noreply.github.com>
2024-08-19 21:06:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ st.write(
 # Load notes data 
 url = 'https://raw.githubusercontent.com/twisted-realms-jess/antusia_notes/main/notes.json'
 response = requests.get(url)
-response_df = st.dataframe(data=response.json(), use_container_width=True)
+st.write(response.json())
+#response_df = st.dataframe(data=response.json(), use_container_width=True)
 
 search_item = st.text_input("Search: ")
 
