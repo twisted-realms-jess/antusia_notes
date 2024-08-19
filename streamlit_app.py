6515a259f642b0975a649c7b0341a51b74ae62d7
twisted-realms-jess <166146094+twisted-realms-jess@users.noreply.github.com>
2024-08-19 18:40:21 +0100

    Add header to streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+# Description: the script that runs the streamlit app
+# Created by: Jess Williamson
+# Created on: 19/08/2024
+
+# Import python packages
+import streamlit
+import pandas
+
+# Set up the app header
+st.title("Welcome to the Antusia notes app")
+st.write("Use the search bar to find what you're looking for"
+
+search_item = st.text_input('Search: ')
+
+
+    
