a99ff1e48e477894c317682295612e2df65aa1a2
twisted-realms-jess <166146094+twisted-realms-jess@users.noreply.github.com>
2024-08-28 20:46:49 +0100

    Set up cache clear

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ import pandas as pd
 import requests
 from io import StringIO
 
+# Clear cache
+st..cache_data.clear()
+
 # Set up the app header
 st.title("Welcome to the Antusia notes app!")
 st.write(
