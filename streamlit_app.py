ce396035f791a073549be7167dd7233a3d41c5fa
twisted-realms-jess <166146094+twisted-realms-jess@users.noreply.github.com>
2024-08-31 11:52:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,7 @@ if search_item:
         st.dataframe(filtered_df.set_index(filtered_df.columns[0]))
     if search_item in session_list:
         st.subheader(search_item + ":")
-        filtered_df = session_df['Notes'].loc[session_df['Session'] == search_item]
+        filtered_df = session_df.loc[session_df['Session'] == search_item]
+        filtered_df = filtered_df['Notes']
         st.dataframe(filtered_df.set_index(filtered_df.columns[0]))
     
