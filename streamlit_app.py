9a36f0ebf58ed51d440f6bd416aa7d98fd75fd96
twisted-realms-jess <166146094+twisted-realms-jess@users.noreply.github.com>
2024-09-01 16:42:14 +0100

    Reformat character details in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write(
     """
 )
 
-# Load notes data 
+# Function to load data
 def load_data(url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -24,21 +24,31 @@ def load_data(url):
     else:
         return st.error('Failed to load data from GitHub.')
 
+# Load character data
 character_df = load_data('https://raw.githubusercontent.com/twisted-realms-jess/antusia_notes/main/characters.csv')
-# st.write(character_df)
 character_list = character_df['Search Term'].unique()
+character_categories = character_df['Category'].unique()
+#character_details = pd.Series(df.Note.values,index=df.Subcategory).to_dict()
 
+# Load session data
 session_df = load_data('https://raw.githubusercontent.com/twisted-realms-jess/antusia_notes/main/sessions.csv')
-# st.write(session_df)
 session_list = session_df['Session'].unique()
 
+# Search item input
 search_item = st.text_input("Search: ")
 
+# Search item display
 if search_item:
     if search_item in character_list:
         st.subheader(search_item + ":")
         filtered_df = character_df.loc[character_df['Search Term'] == search_item]
-        st.dataframe(filtered_df.set_index(filtered_df.columns[0]))
+        for category in character_categories:
+            st.write(category + ":")
+            details = filtered_df[filtered_df['Category'] == category]
+            character_details = pd.Series(details.Note.values,index=details.Subcategory).to_dict()
+            for key, value in character_details.items():
+                st.write(key + " = " + value)
+        #st.dataframe(filtered_df.set_index(filtered_df.columns[0]),use_container_width=True)
     if search_item in session_list:
         st.subheader(search_item + ":")
         filtered_df = session_df.loc[session_df['Session'] == search_item]
